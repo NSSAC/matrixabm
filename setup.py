9faeff9d39597f00fdd59f0a91044a459db4c401
Parantapa Bhattacharya <pb@parantapa.net>
2020-05-07 13:20:12 -0400

    Add better classifiers in setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,18 @@ description = "The Matrix ABM: An Agent Based Modeling Framework"
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
+classifiers = """
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+License :: OSI Approved :: MIT License
+Operating System :: POSIX :: Linux
+Programming Language :: C
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Topic :: Scientific/Engineering
+Topic :: System :: Distributed Computing
+""".strip().split("\n")
+
 setup(
     name=package_name,
     description=description,
@@ -37,8 +49,5 @@ setup(
     ],
 
     url="http://github.com/nssac/matrixabm",
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-    ),
+    classifiers=classifiers
 )
